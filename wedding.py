a05b2e230d19ab3a16829f1e5e68cabc4acce678
Sriram Raman <sriram.raman@gmail.com>
2012-01-16 12:10:59 -0500

    Make wedding.py handle requests to all pages.

--- wedding.py
+++ wedding.py
@@ -1,4 +1,5 @@
 #!/usr/lib/python2.5
+
 import logging, os, time, webapp2
 from google.appengine.ext.webapp import template
 
@@ -6,12 +7,10 @@ class MainPage(webapp2.RequestHandler):
   def get(self):
       self.response.headers['Content-Type'] = 'text/html'
       path = os.path.join(os.path.dirname(__file__), 'index.html')
-      
+
       start_rt = time.time()
       rt = template.render(path, {})  # rendered template
       logging.info("LOG: Rendering main page took: %s secs" , time.time()-start_rt)
-      print "P: Rendering main page took: %s secs" , time.time()-start_rt)
-      sys.stdout.write("Rendering main page took: %s secs" , time.time()-start_rt))
       self.response.out.write(rt)
 
 class RSVPPage(webapp2.RequestHandler):
@@ -21,14 +20,5 @@ class RSVPPage(webapp2.RequestHandler):
     self.response.out.write(template.render(path,{}))
 
 
-def main(argv):
-  logging.getLogger().setLevel(logging.DEBUG)
-  app = webapp2.WSGIApplication([('/', MainPage),
-                                 ('/rsvp', RSVPPage)],
-                                debug=True)
-  
-  sys.stdout.write("FUCK")
-
-
-if __name__ == '__main__':
-  main(sys.argv)
+app = webapp2.WSGIApplication([('/', MainPage),
+                               ('/rsvp', RSVPPage)])
