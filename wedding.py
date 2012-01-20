66595b5ab7819798369d4369ebffb5c6231c0d1c
Sriram Raman <sriram.raman@gmail.com>
2012-01-19 23:08:33 -0500

    RSVP code update.

--- wedding.py
+++ wedding.py
@@ -8,19 +8,35 @@ class MainPage(webapp2.RequestHandler):
   def get(self):
       self.response.headers['Content-Type'] = 'text/html'
       path = os.path.join(os.path.dirname(__file__), 'index.html')
-
-      start_rt = time.time()
       rt = template.render(path, {})  # rendered template
-      logging.info("LOG: Rendering main page took: %s secs" , time.time()-start_rt)
       self.response.out.write(rt)
 
-class RSVPPage(webapp2.RequestHandler):
+class Guest(db.Model):
+  name = db.StringProperty(required=True)
+  coming = db.BooleanProperty(required=True)
+  message = db.StringProperty()
+  
+class RSVPConf(webapp2.RequestHandler):
   def get(self):
-    self.request.get('g
+    fullname = self.request.get('fullname')
+    if not fullname:
+      self.redirect('/rsvp.html')
+
+    coming = self.request.get('coming')
+    message = self.request.get('message')
+    
+    g = Guest(name=fullname, coming=coming, message=message)
+    g.put()
+    self.response.out.write('%s: coming: %s, message: %s' % (
+        fullname, coming, message))
+
+class RSVPHTMLPage(webapp2.RequestHandler):
+  def get(self):
     self.response.headers['Content-Type'] = 'text/html'
     path = os.path.join(os.path.dirname(__file__), 'rsvp.html')
     self.response.out.write(template.render(path,{}))
 
 
 app = webapp2.WSGIApplication([('/', MainPage),
-                               ('/rsvp', RSVPPage)])
+                               ('/rsvp', RSVPConf),
+                               ('/rsvp.html', RSVPHTMLPage)])
